cd2ab5725463e3feae75ae4728ac2a3b450de64e
Ziyoxuja <ziyoxuja90@gmail.com>
2023-07-19 21:53:18 +0500

    django

--- books/urls.py
+++ books/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path , include
 from books import views
 urlpatterns = [
-    path("",views.home),
-    path("main.html",views.main)
+    path("",views.home ,name='home'),
+    path("doctor",views.doctor,name="doctor"),
+    path("about",views.about,name="about"),
+    path("contact",views.contact,name="contact"),
+    path("testimonial",views.testimonial,name="testimonial"),
+    path("treatment",views.treatment,name="treatment"),
+
 ]
 
--- books/views.py
+++ books/views.py
@@ -4,5 +4,19 @@ from django.shortcuts import render
 def home(request):
     return render(request , 'index.html')
 
-def main(request):
-    return render(request , 'main.html')
+def doctor(request):
+    return render(request , 'doctor.html')
+
+
+def about(request):
+    return render(request , 'about.html')
+
+
+def contact(request):
+    return render(request , 'contact.html')
+
+def testimonial(request):
+    return render(request , 'testimonial.html')
+
+def treatment(request):
+    return render(request , 'treatment.html')
